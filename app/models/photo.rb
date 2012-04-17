f6862d6d3a803ccdbaa257eb620b66f41588f5eb
Matt Zukowski <matt@zukowski.ca>
2012-04-17 18:03:32 -0400

    include url in Photo json serialization

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,7 @@ class Photo < ActiveRecord::Base
   attr_protected :id
 
   belongs_to :of_object, :polymorphic => true
-  has_attached_file :image, :path => ":rails_root/public/photos/:attachment/:id/:style/:filename.jpg"
+  has_attached_file :image, 
+    :path => ":rails_root/public/photos/:attachment/:id/:style/:filename.jpg",
+    :url => "/photos/:attachment/:id/:style/:filename.jpg"
 end
