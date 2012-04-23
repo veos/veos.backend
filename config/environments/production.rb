c33e44ab14791eb7687778cb41c4fded97d7335f
Matt Zukowski <matt@zukowski.ca>
2012-04-23 14:11:33 -0400

    enable asset compilation and debugging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,17 +5,17 @@ VeosBackend::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true # TODO: disable this once we launch!
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
