d071e7c8addaae67f24c0e2edd615a47eab1656d
Matt Zukowski <matt@zukowski.ca>
2012-05-10 14:08:30 -0400

    make sure that the report has an 'about' (camera or sign)

--- app/models/report.rb
+++ app/models/report.rb
@@ -9,6 +9,23 @@ class Report < ActiveRecord::Base
   accepts_nested_attributes_for :camera
   accepts_nested_attributes_for :sign
 
-  validates_presence_of :loc_lat_from_gps, :loc_lng_from_gps, 
+  validates_associated :camera, :if => :camera
+  validates_associated :sign, :if => :sign
+
+  validates :about, :presence => true
+  validates :loc_lat_from_gps, :loc_lng_from_gps, :presence => true,
     :if => Proc.new {|r| r.loc_lat_from_user.blank? && r.loc_lng_from_user.blank?}
+
+  def about
+    sign || camera
+  end
+
+  # this doesn't actually do anything; about assignment is done via self.camera= or self.sign=
+  def about=(about)
+    unless about.to_s == "camera" || about.to_s == "sign"
+      errors.add(:about, "must be a sign or a camera but a "+about+" was given.")
+    end
+  end
+
+
 end
