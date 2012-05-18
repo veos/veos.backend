2dfca6b5a88e81ba2242672b5e63f7db41053eb6
Matt Zukowski <matt@zukowski.ca>
2012-05-18 13:01:47 -0400

    removed 'about' method from Report model since a report can now be both about a Sign and a Camera

--- app/models/report.rb
+++ app/models/report.rb
@@ -12,20 +12,7 @@ class Report < ActiveRecord::Base
   validates_associated :camera, :if => :camera
   validates_associated :sign, :if => :sign
 
-  validates :about, :presence => true
   validates :loc_lat_from_gps, :loc_lng_from_gps, :presence => true,
     :if => Proc.new {|r| r.loc_lat_from_user.blank? && r.loc_lng_from_user.blank?}
-
-  def about
-    sign || camera
-  end
-
-  # this doesn't actually do anything; about assignment is done via self.camera= or self.sign=
-  def about=(about)
-    unless about.to_s == "camera" || about.to_s == "sign"
-      errors.add(:about, "must be a sign or a camera but a "+about+" was given.")
-    end
-  end
-
 
 end
