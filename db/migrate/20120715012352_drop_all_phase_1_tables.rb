ea74e7ccf63553a7d94c9b7e1de97176fbe00718
Matt Zukowski <matt@zukowski.ca>
2012-07-17 15:25:27 -0400

    don't try to delete sign_ tables that don't exist

--- db/migrate/20120715012352_drop_all_phase_1_tables.rb
+++ db/migrate/20120715012352_drop_all_phase_1_tables.rb
@@ -3,8 +3,6 @@ class DropAllPhase1Tables < ActiveRecord::Migration
     drop_table :camera_spaces
     drop_table :cameras
     drop_table :photos
-    drop_table :sign_stated_properties
-    drop_table :sign_stated_purposes
     drop_table :signs
     drop_table :reports
   end
