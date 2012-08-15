43846c33a5008c33762f63d07017de09ce0a9f3e
Matt Zukowski <matt@zukowski.ca>
2012-08-15 13:35:08 -0400

    ignore sign_stated_purpose and sign_properties attributes... client shouldn't be sending these

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,14 +1,14 @@
 class Report < ActiveRecord::Base
   attr_protected :id, 
     :installation_attributes, # hacky way to ignore installation stuff, since we don't want this updated via nested attributes
-    :surveilled_space # client is sending this for some reason... probably client-side bug, but patching it here for now
+    :surveilled_space, :sign_stated_purpose, :sign_properties # client is sending these for some reason... probably client-side bug, but patching it here for now
 
   belongs_to :installation
   has_many :photos
   has_many :tags, :class_name => ReportTag.name
 
-  accepts_nested_attributes_for :photos
-  accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :photos, :allow_destroy => true
+  accepts_nested_attributes_for :tags, :allow_destroy => true
 
   validates :loc_lat_from_gps, :loc_lng_from_gps, :presence => true,
     :if => Proc.new {|r| r.loc_lat_from_user.blank? && r.loc_lng_from_user.blank?}
