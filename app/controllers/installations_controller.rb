8e0bf2ec88e2741146dce4156d7322b2c3a5b4d5
Matt Zukowski <matt@zukowski.ca>
2012-10-01 14:41:59 -0400

    installations with no owner name are sorted last; identical owner names are sorted by modification date, with newest first

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -14,7 +14,20 @@ class InstallationsController < ApplicationController
       @inst = Installation.find(:all,
         :include => :latest_report)
     end
-    @inst.sort_by!{|inst| inst.owner_name || ""}
+
+    # sort by owner_name, blank last
+    # if names are identical, installations with newer reports go first
+    @inst.sort! do |a,b| 
+      if a.owner_name.blank?
+        1
+      elsif b.owner_name.blank?
+        -1
+      else
+        [a.owner_name, a.latest_report && Time.now - a.latest_report.updated_at] <=> 
+          [b.owner_name, b.latest_report && Time.now - b.latest_report.updated_at]
+      end
+    end
+
     respond_with_installation(@inst)
   end
 
