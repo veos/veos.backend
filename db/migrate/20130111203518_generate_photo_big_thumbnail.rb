a296fbbb7b475f76ce5b2631eaf99b347505f7fa
Matt Zukowski <matt@zukowski.ca>
2013-01-11 15:45:34 -0500

    rescue from bad reprocess on photos with invalid filename

--- db/migrate/20130111203518_generate_photo_big_thumbnail.rb
+++ db/migrate/20130111203518_generate_photo_big_thumbnail.rb
@@ -1,6 +1,11 @@
 class GeneratePhotoBigThumbnail < ActiveRecord::Migration
   def up
-    Photo.all.each {|p| p.image.reprocess! if p.image }
+    say_with_time("Generting big thumbnails...") do
+      Photo.all.each do |p|
+        say "#{p.id}: p.image"
+        (p.image.reprocess! if p.image) Errno::ENOENT
+      end
+    end
   end
 
   def down
