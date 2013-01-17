58653911d51cc6cf7cb256671e59887bfe4c1d55
VEOS <veos.team@gmail.com>
2013-01-17 16:49:04 -0500

    after rack db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121030205526) do
+ActiveRecord::Schema.define(:version => 20130111203518) do
 
   create_table "installations", :force => true do |t|
     t.string  "compliance_level_override"
